5f276a74cdec7e1b875c80737f7c2a5150aa7acc
Diego <diegozamora995@gmail.com>
2025-08-26 03:12:08 -0500

    quitando reportes del menu

--- menu.py
+++ menu.py
@@ -5,7 +5,7 @@ def mostrar_menu():
     opciones = {
         "Inicio": home.mostrar,
         "Simulador de créditos": simulador.mostrar,
-        "Reportes": reportes.mostrar
+        
     }
 
     seleccion = st.sidebar.selectbox("Navegación", list(opciones.keys()))
